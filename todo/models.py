ab67b6264dd8eaadf387eeb6d8f5f1760f73a2b9
Florin M <florinmaracine@yahoo.com>
2024-12-27 08:46:04 +0000

    Override string method in Item model to display item name in the list

--- todo/models.py
+++ todo/models.py
@@ -5,5 +5,6 @@ class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
     
+    # override default string method to display the name of the itemin the Items list
     def __str__(self):
         return self.name
